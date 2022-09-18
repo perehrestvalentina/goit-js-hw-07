4af1c00d60c84302890029cba2aedc4a684121f6
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-09-18 19:54:46 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// const refs = {
-//     gallery: document.querySelector(".gallery"),
-//     image: document.createElement("img"),
-//     lightbox: document.querySelector(".lightbox"),
-//     btn: document.querySelector('[data-action="close-lightbox"]'),
-//     modal: document.querySelector(".lightbox__content"),
-//     lightbox__image: document.querySelector(".lightbox__image"),
-//   };
 
-const createGalleryItems = ((previev, oridginal, description)=>{
-    `<div class="gallery__item">
- <a class="gallery__link" href="large-image.jpg">
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return`<div class="gallery__item">
+ <a class="gallery__link" href="${original}">
      <img
        class="gallery__image"
-       src="${previev}"
-       data-source="${oridginal}"
+       src="${preview}"
+       data-source="${original}"
        alt="${description}"
      />
    </a>
- </div>`
-});
-console.log(galleryItems);
+ </div>`;
+})
+.join('');
+  }
 
 
-// // document.addEventListener('scroll', handleScroll);
+console.log(galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+ 
+  const isGalleryEl = event.target.classList.contains("gallery__image");
+
+  if(!isGalleryEl){
+    return;
+  }
+  const oridginalImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${oridginalImg}" width="800" height="600">`);
+
+   instance.show();
+  
+};
+
+
+
+
 // const galleryMarkup = images.reduce(
 //     (acc, item) => acc + createGalleryItem(item),
 //     ""
@@ -36,35 +56,37 @@ console.log(galleryItems);
 //   refs.btn.addEventListener("click", onClickHandlerClose);
 //   refs.modal.addEventListener("click", closeLightbox);
   
-//   function onGalleryClick(e) {
-//     e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') {
-//       return;
-//     }
-//     if (e.target.nodeName === "IMG") {
-//       refs.lightbox.classList.add("is-open");
-//       refs.lightbox__image.src = e.target.getAttribute("data-source");
-//       refs.lightbox__image.alt = e.target.alt;
-//     }
-//     window.addEventListener("keyup", clickKey);
-//   }
+  // function onGalleryClick(e) {
+  //   e.preventDefault();
+  //   if (e.target.nodeName !== 'IMG') {
+  //     return;
+  //   }
+  //   if (e.target.nodeName === "IMG") {
+  //     refs.lightbox.classList.add("is-open");
+  //     refs.lightbox__image.src = e.target.getAttribute("data-source");
+  //     refs.lightbox__image.alt = e.target.alt;
+  //   }
+  //   window.addEventListener("keyup", clickKey);
+  // }
+  btn.addEventListener("click", onClickHandlerClose);
+  modal.addEventListener("click", closeLightbox);
   
-//   function onClickHandlerClose(e) {
-//     e.preventDefault(); 
-//     refs.lightbox.classList.remove("is-open");
-//     refs.lightbox__image.src = '';
-//     refs.lightbox__image.alt = '';
-//     window.removeEventListener("keyup", clickKey);
-//   }
+  function onClickHandlerClose(event) {
+    event.preventDefault(); 
+    refs.lightbox.classList.remove("is-open");
+    refs.lightbox__image.src = '';
+    refs.lightbox__image.alt = '';
+    window.removeEventListener("keyup", clickKey);
+  }
   
-//   function closeLightbox(event) {
-//     if (event.target === event.currentTarget) {
-//       onClickHandlerClose();
-//     }
-//   }
+  function closeLightbox(event) {
+    if (event.target === event.currentTarget) {
+      onClickHandlerClose();
+    }
+  }
   
-//   function clickKey(event) {
-//     if (event.code === "Escape") {
-//       onClickHandlerClose();
-//     }
-//   }
+  function clickKey(event) {
+    if (event.code === "Escape") {
+      onClickHandlerClose();
+    }
+  }
