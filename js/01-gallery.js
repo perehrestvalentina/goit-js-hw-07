50f635b4e54fef04ed10228c188df415211ee23c
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-09-19 19:19:32 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log (galleryItems);
 
 const galleryImg = document.querySelector('.gallery');
+const imagesMarkup = createListGallery(galleryItems);
+galleryImg.insertAdjacentHTML('beforeend', imagesMarkup);
 
-const imagesMarkup = galleryItems
-.map(({preview,original,description}) => 
-`<div class = "gallery__item">
+function createListGallery(galleryItems){
+    return galleryItems
+.map(({preview, original, description}) => {
+return `<div class = "gallery__item">
 <a class = "gallery__link" href = "${original}">
-<img class = "gallery__image"
+<img 
+class = "gallery__image"
   src = "${preview}"
   data-source = "${original}"
   alt = "${description}"/>
-</a></div>`).join("");
+</a></div>`}).join("");
 
-galleryImg.insertAdjacentHTML('begineend', imagesMarkup);
+}
 galleryImg.addEventListener('click', onImagesClick);
 
 function onImagesClick(event) {
